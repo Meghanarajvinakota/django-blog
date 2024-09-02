e1416cc940dab2fbeda6ea02b5c15fff4360bfb0
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-09-02 14:14:40 +0000

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -16,4 +16,33 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-   
+    field_2 = models.CharField(default='Hello, World!')
+    field_3 = models.CharField(null = True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"The title of this post is {self.title}"
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    challenge = models.SlugField()
+    
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
